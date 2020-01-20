2a72afb6baf7fff329019aac7defe5e90f55be7d
Douglas Henrique <doughrqe@gmail.com>
2020-01-19 23:46:24 -0300

    Concluido requisitos 1,2,3

--- Douglas/script.js
+++ Douglas/script.js
@@ -1,5 +1,3 @@
-// window.onload = function onload() { };
-
 function createProductImageElement(imageSource) {
   const img = document.createElement('img');
   img.className = 'item__image';
@@ -43,3 +41,34 @@ function createCartItemElement({ sku, name, salePrice }) {
 }
 
 const NOME = document.querySelector('.input-name');
+NOME.addEventListener('change', ()=>{
+  sessionStorage.setItem('Nome', NOME.value);
+})
+
+const check = document.querySelector('.input-terms');
+check.addEventListener('click', ()=> {
+  document.cookie= `Checkbox = ${check.checked}`;
+})
+
+const API_URL = 'https://api.mercadolibre.com/sites/MLB/search?q=retrovisor';
+
+const teste = fetch(API_URL, {
+    headers: ({
+        method : 'GET',
+        Accept : 'application/json',
+})})
+.then(response => {
+    response.json()
+    .then(response => {
+      const add = document.querySelector('.items');
+      const objeto ={
+        sku : response.results[0].id,
+        name : response.results[0].title,
+        image: response.results[0].thumbnail,
+      }
+      add.appendChild(createProductItemElement(objeto))
+    })
+    .catch(() => console.error('Não foi possivel encontrar o produto'))
+})
+.catch(()=> console.error('Não foi possivel encontrar o produto'))
+
