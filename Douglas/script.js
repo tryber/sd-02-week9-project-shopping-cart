ff44de2a543994aaa34897a867bf50be0f61f737
Douglas Henrique <doughrqe@gmail.com>
2020-01-19 21:59:37 -0300

    Alteração do Onload

--- Douglas/script.js
+++ Douglas/script.js
@@ -1,4 +1,4 @@
-window.onload = function onload() { };
+// window.onload = function onload() { };
 
 function createProductImageElement(imageSource) {
   const img = document.createElement('img');
@@ -41,3 +41,5 @@ function createCartItemElement({ sku, name, salePrice }) {
   li.addEventListener('click', cartItemClickListener);
   return li;
 }
+
+const NOME = document.querySelector('.input-name');
