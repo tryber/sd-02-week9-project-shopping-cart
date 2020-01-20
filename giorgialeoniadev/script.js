30c3bff6d6e517d3652fac50a906389d6264c3c9
Giorgia <giorgialeoniac@gmail.com>
2020-01-20 19:36:02 -0300

    Feito o primeiro requisito - Adicionando o nome ao sesstionStorage

--- giorgialeoniadev/script.js
+++ giorgialeoniadev/script.js
@@ -1,4 +1,4 @@
-window.onload = function onload() { };
+window.onload = function onload() {};
 
 function createProductImageElement(imageSource) {
   const img = document.createElement('img');
@@ -14,7 +14,11 @@ function createCustomElement(element, className, innerText) {
   return e;
 }
 
-function createProductItemElement({ sku, name, image }) {
+function createProductItemElement({
+  sku,
+  name,
+  image
+}) {
   const section = document.createElement('section');
   section.className = 'item';
 
@@ -34,10 +38,23 @@ function cartItemClickListener(event) {
   // coloque seu código aqui
 }
 
-function createCartItemElement({ sku, name, salePrice }) {
+function createCartItemElement({
+  sku,
+  name,
+  salePrice
+}) {
   const li = document.createElement('li');
   li.className = 'cart__item';
   li.innerText = `SKU: ${sku} | NAME: ${name} | PRICE: $${salePrice}`;
   li.addEventListener('click', cartItemClickListener);
   return li;
 }
+
+//1. Salve o nome da pessoa no SessionStorage
+//Você deve salvar o nome da pessoa que utiliza a página na SessionStorage. A pessoa deve digitar o nome dela no campo <input class="input-name" type="text"> (já presente na página).
+const name = document.getElementsByClassName('input-name')[0];
+name.addEventListener('keyup', (event) => {
+  if (event.keyCode === 13) {
+    sessionStorage.setItem('Nome', `${name.value}`)
+  }
+});
