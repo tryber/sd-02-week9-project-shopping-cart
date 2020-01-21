4dcc8a988bafa04c671715ed7ca603f4f0baaf3a
guicgs <etohlif@protonmail.com>
2020-01-21 18:39:47 -0300

    requisito 4 concluído

--- guicgs/script.js
+++ guicgs/script.js
@@ -47,44 +47,44 @@ function createProductItemElement({ sku, name, image }) {
   section.appendChild(createCustomElement('span', 'item__title', name));
   section.appendChild(createProductImageElement(image));
   section.appendChild(createCustomElement('button', 'item__add', 'Adicionar ao carrinho!'));
-  document.querySelector('.items').appendChild(section);
+
   return section;
 }
 
+function getSkuFromProductItem(item) {
+  return item.querySelector('span.item__sku').innerText;
+}
+
 const searchInput = document.getElementsByClassName('input-search')[0];
-console.log(searchInput)
 searchInput.addEventListener('keyup', (event) => {
   if (event.keyCode === 13) {
     clearPreviousSearch();
     fetch(`https://api.mercadolibre.com/sites/MLB/search?q=${searchInput.value}`)
       .then(response => response.json().then((responseObj) => {
         responseObj.results.forEach(element =>
-          createProductItemElement({
+          document.querySelector('.items').appendChild(createProductItemElement({
             sku: element.id,
             name: element.title,
             image: element.thumbnail,
-          }));
+          })));
       }))
+      .then(() => {
+        const id = document.querySelector('.items');
+        const addCart = document.querySelectorAll('.item__add');
+        addCart.forEach(element => element.addEventListener('click', () => {
+          fetch(`https://api.mercadolibre.com/items/${getSkuFromProductItem(id)}`)
+            .then(response => response.json().then(object => {
+              document.querySelector('.cart__items').appendChild(createCartItemElement({
+                sku: object.id,
+                name: object.title,
+                salePrice: object.price,
+              }));
+            }))
+        }))
+      })
       .catch(error => console.log(error));
   }
 });
-
-const addCart = document.querySelector('.item__add');
-addCart.addEventListener('click', () => {
-  fetch(`https://api.mercadolibre.com/items/${getSkuFromProductItem()}`)
-    .then(response => response.json().then(object => {
-      createCartItemElement({
-        sku: object.id,
-        name: object.title,
-        salePrice: object.price,
-      });
-    }))
-})
-
-
-function getSkuFromProductItem(item) {
-  return item.querySelector('span.item__sku').innerText;
-}
 
 function cartItemClickListener(event) {
   // coloque seu código aqui
