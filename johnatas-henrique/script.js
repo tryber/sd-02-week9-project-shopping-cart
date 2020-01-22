917419442846ebef67c302919624578e3e063586
Johnatas Henrique <johnatas.henrique@gmail.com>
2020-01-22 01:26:58 -0300

    Requisito 3 e 4

--- johnatas-henrique/script.js
+++ johnatas-henrique/script.js
@@ -1,25 +1,58 @@
-function createProductImageElement(imageSource) {
-  const img = document.createElement('img');
-  img.className = 'item__image';
-  img.src = imageSource;
-  return img;
+const headers = {
+  Accept: 'application/json'
+};
+const QUERY = 'computador';
+let resultados = [];
+const url = `https://api.mercadolibre.com/sites/MLB/search?q=${QUERY}`;
+
+function pegaLista(url, headers) {
+  fetch(url, headers)
+  .then(res => {
+    res.json()
+    .then(data => {
+      resultados = data.results;
+          mostrarProdutos();   
+        })
+      })
+      .catch(() => console.log('Erro'));
+    }
+
+function pegaProduto(url, headers) {
+  fetch(url, headers)
+    .then(res => {
+      res.json()
+        .then(data => {
+          const produto = data;
+          const objProduto = createCartItemElement({
+            sku: produto.id,
+            name: produto.title,
+            salePrice: produto.thumbnail
+          });
+          const itensCarrinho = document.querySelector('.cart__items');
+          itensCarrinho.appendChild(objProduto);
+        })
+    })
+    .catch(() => console.log('Erro pega Produto'));
 }
 
-function createCustomElement(element, className, innerText) {
-  const e = document.createElement(element);
-  e.className = className;
+    function createProductImageElement(imageSource) {
+      const img = document.createElement('img');
+      img.className = 'item__image';
+      img.src = imageSource;
+      return img;
+    }
+    
+    function createCustomElement(element, className, innerText) {
+      const e = document.createElement(element);
+      e.className = className;
   e.innerText = innerText;
   return e;
 }
 
-function createProductItemElement({
-  sku,
-  name,
-  image
-}) {
+function createProductItemElement({ sku, name, image }) {
   const section = document.createElement('section');
   section.className = 'item';
-
+  
   section.appendChild(createCustomElement('span', 'item__sku', sku));
   section.appendChild(createCustomElement('span', 'item__title', name));
   section.appendChild(createProductImageElement(image));
@@ -36,11 +69,7 @@ function cartItemClickListener(event) {
   // coloque seu código aqui
 }
 
-function createCartItemElement({
-  sku,
-  name,
-  salePrice
-}) {
+function createCartItemElement({ sku, name, salePrice }) {
   const li = document.createElement('li');
   li.className = 'cart__item';
   li.innerText = `SKU: ${sku} | NAME: ${name} | PRICE: $${salePrice}`;
@@ -65,13 +94,39 @@ const cookieChecked = () => {
   const cookieName = 'checkbox';
   inputTerms.addEventListener('click', () => {
     inputTerms.checked ?
-      document.cookie = `${cookieName}=true; ${cookieTime}` :
-      document.cookie = `${cookieName}=false; ${cookieTime}`;
+    document.cookie = `${cookieName}=true; ${cookieTime}` :
+    document.cookie = `${cookieName}=false; ${cookieTime}`;
   });
   if (document.cookie.includes(`${cookieName}=true`)) inputTerms.checked = true;
 }
 
+// Requisito 3
+const mostrarProdutos = () => {
+  const listaItems = document.querySelector('.items');
+  resultados.forEach(resultado => {
+    const objProdutos = createProductItemElement(
+      { sku: resultado.id, name: resultado.title, image: resultado.thumbnail});
+      listaItems.appendChild(objProdutos);
+    })
+    adicionarProduto();
+  }
+  
+  //Requisito 4
+  
+  const adicionarProduto = () => {
+    const adiItems = document.querySelectorAll('.item__add');
+    adiItems.forEach(adiItem => adiItem.addEventListener('click', (event) => {
+      const ItemID = getSkuFromProductItem(event.target.parentNode);
+      const endpoint = `https://api.mercadolibre.com/items/${ItemID}`;
+      const teste = pegaProduto(endpoint, headers);
+      console.log(teste);
+}));
+}
+
+
 window.onload = function onload() {
   nomeSessionStorage();
   cookieChecked();
+  pegaLista(url, headers);
+  
 };
