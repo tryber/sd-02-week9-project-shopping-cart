eaa33849b87c60a6e8616b59194b5eeaa8783642
laurolyra <laurolyra@gmail.com>
2020-01-23 03:05:56 -0300

    atualizações com a branch master

--- Lauro/script.js
+++ Lauro/script.js
@@ -106,7 +106,6 @@ function carregaStorage() {
     }
   }
 }
-
 function createCartItemElement({ sku, name, salePrice }) {
   const li = document.createElement('li');
   li.className = 'cart__item';
