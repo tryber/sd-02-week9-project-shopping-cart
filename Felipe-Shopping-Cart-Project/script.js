d21c105715a259c4bc0f349d92bee6b1ab3882cc
EddyeBoy27 <lipe0503@gmail.com>
2020-01-24 02:36:46 -0200

    Finaizado LocalStorage, Iniciando Carrinho de Compras Com Itens do LocalStorage

--- Felipe-Shopping-Cart-Project/script.js
+++ Felipe-Shopping-Cart-Project/script.js
@@ -1,3 +1,5 @@
+let contador = 1;
+
 // Inicio Requisito 1
 
 function salvaSession() {
@@ -58,6 +60,15 @@ function botaoAdiciona() {
           .appendChild(
             createCartItemElement(
               { sku: item.id, name: item.title, salePrice: item.price }));
+        if (localStorage.getItem(item.title) === null) {
+          localStorage
+            .setItem(item.title, JSON.stringify
+              ({id: item.id, title: item.title, salePrice: item.price, count: contador = 1 }));
+        } else {
+            localStorage
+              .setItem(item.title, JSON.stringify
+                ({id: item.id, title: item.title, salePrice: item.price, count: contador++ }));
+          }
         });
       });
     });
